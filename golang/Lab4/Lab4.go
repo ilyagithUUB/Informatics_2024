9c87363eee7df6c0248ee76e0b1555ff173bf34c
ilyagithUUB <ilyagithubvityabratan@gmail.com>
2024-11-04 21:30:17 +0300

    Lab 4

--- golang/Lab4/Lab4.go
+++ golang/Lab4/Lab4.go
@@ -1,10 +1,19 @@
 package lab4
 
 import (
+	"fmt"
 	"math"
 )
 
 func Calculate(x float64) float64 {
 	y := math.Pow(math.Pow(math.Asin(x), 2)+math.Pow(math.Acos(x), 4), 3)
 	return y
+}
+
+func main() {
+	xValues := []float64{0.1, 0.35, 0.4, 0.55, 0.6}
+	for _, x := range xValues {
+		y := math.Pow(math.Pow(math.Asin(x), 2)+math.Pow(math.Acos(x), 4), 3)
+		fmt.Printf("x = %.2f, y = %.6f\n", x, y)
+	}
 }
